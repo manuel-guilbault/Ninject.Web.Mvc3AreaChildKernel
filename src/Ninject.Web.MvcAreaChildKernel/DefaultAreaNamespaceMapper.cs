0351f937d647284296550075f5a2a52c7f68d36d
Manuel Guilbault <manuel.guilbault@gmail.com>
2013-12-25 17:57:58 +0100

    Added support for namespace patterns.

--- src/Ninject.Web.MvcAreaChildKernel/DefaultAreaNamespaceMapper.cs
+++ src/Ninject.Web.MvcAreaChildKernel/DefaultAreaNamespaceMapper.cs
@@ -23,19 +23,31 @@ namespace Ninject.Web.MvcAreaChildKernel
         {
             string areaName;
 
-            var parts = @namespace.Split('.');
-            var index = parts.Length;
-            while (index > 0)
+            // Try to directly resolve the namespace.
+            if (namespaceToAreaNameMap.TryGetValue(@namespace, out areaName))
             {
-                var @matchedNamespace = string.Join(".", parts.Take(index));
-                if (namespaceToAreaNameMap.TryGetValue(@matchedNamespace, out areaName))
+                return areaName;
+            }
+
+            // Walk the parent namespaces and try to resolve the area name using the wildcard pattern.
+            // For example, if @namespace is "MvcSample.Areas.Admin.Controllers" and a map entry
+            // exists for "MvcSample.Areas.Admin.*", it will be matched.
+            var parts = @namespace.Split('.');
+            var patternLength = parts.Length;
+            while (patternLength > 0)
+            {
+                var patternParts = parts.Take(patternLength).Concat(new[] { "*" });
+                var pattern = string.Join(".", patternParts);
+                if (namespaceToAreaNameMap.TryGetValue(pattern, out areaName))
                 {
                     cache.Map(@namespace, areaName);
                     return areaName;
                 }
 
-                --index;
+                --patternLength;
             }
+            
+            // Cannot resolve the namespace.
             return null;
         }
 
@@ -46,20 +58,14 @@ namespace Ninject.Web.MvcAreaChildKernel
             if (namespaces == null) throw new ArgumentNullException("namespaces");
             if (!namespaces.Any()) throw new ArgumentException("namespaces cannot be empty", "namespaces");
 
-            foreach (var @namespace in namespaces)
-            {
-                if (namespaceToAreaNameMap.ContainsKey(@namespace))
-                {
-                    throw new ArgumentException(string.Format("Area already registered for namespace '{0}'", @namespace), "namespaces");
-                }
-            }
+            var existingNamespace = namespaces.FirstOrDefault(ns => namespaceToAreaNameMap.ContainsKey(ns));
+            if (existingNamespace != null) throw new ArgumentException(string.Format("Area already registered for namespace '{0}'", existingNamespace), "namespaces");
 
             areaNames.Add(areaName);
             foreach (var @namespace in namespaces)
             {
                 namespaceToAreaNameMap.Add(@namespace, areaName);
             }
-
             cache.Clear();
         }
 
